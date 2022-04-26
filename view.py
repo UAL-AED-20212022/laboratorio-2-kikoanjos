aed1c07264ef055d52b10be8c8dc6f73bba348b0
Rodrigo Anjos <30009487@students.ual.pt>
2022-04-26 10:15:59 +0100

    1

--- view.py
+++ view.py
@@ -13,40 +13,40 @@ def main():
         if commands[0] == "RPI":
             lista_paises.insert_at_start(commands[1])
         if commands[0] == "RPF":
-
+            pass
         if commands[0] == "RPDE":
-        
+            pass
         if commands[0] == "RPAE":
-
+            pass
         if commands[0] == "RPII":
-
+            pass
         if commands[0] == "VNE":
-
+            pass
         if commands[0] == "VP":
-
+            pass
         if commands[0] == "EPE":
-
+            pass
         if commands[0] == "EUE":
-
+            pass
         if commands[0] == "EP":
-    
+            pass
 def registar_pais_inicio():
-
+    pass
 def registar_pais_fim():
-
+    pass
 def registar_pais_depois_de_outro():
-
+    pass
 def registar_pais_antes_de_outro():
-
+    pass
 def registar_pais_num_local_especifico():
-
+    pass
 def verificar_numero_elementos():
-
+    pass
 def verificar_pais_na_lista():
-
+    pass
 def eliminar_primeiro_pais():
-
+    pass
 def eliminar_ultimo_pais():
-
+    pass
 def eliminar_pais_especifico():
-
+    pass
