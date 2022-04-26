1baf2deaea6abbf5f267651a818dc87b95d350f5
kikoanjos <rodrigoanjos10@gmail.com>
2022-04-26 15:15:05 +0100

    Final

--- program.py
+++ program.py
@@ -4,3 +4,5 @@ import sys
 sys.stdout.reconfigure(encoding = 'UTF-8')
 if __name__ == '__main__':
     view.main()
+
+    #Utilização de listas ligadas com a utilização de nomes de países
--- view.py
+++ view.py
@@ -8,28 +8,36 @@ def main():
         except EOFError:
             return
 
+        #Inserir país no início
         if commands[0] == "RPI":
             lista_paises.insert_at_start(commands[1])
             lista_paises.traverse_list()
 
+        #Inserir país no fim
         if commands[0] == "RPF":
             lista_paises.insert_at_end(commands[1])
             lista_paises.traverse_list()
 
+        #Inserir país depois de outro
         if commands[0] == "RPDE":
             lista_paises.insert_after_item(commands[2], commands[1])
             lista_paises.traverse_list()
 
+        #Inserir país antes de outro
         if commands[0] == "RPAE":
             lista_paises.insert_before_item(commands[2], commands[1])
             lista_paises.traverse_list()
+
+        #Inserir país num lugar especifico
         if commands[0] == "RPII":
             lista_paises.insert_at_index(int(commands[2]), commands[1])
             lista_paises.traverse_list()
 
+        #Verificar quantos países existem
         if commands[0] == "VNE":
             print(f"O número de elementos são {lista_paises.get_count()}.")
 
+        #Verificar se existe o país
         if commands[0] == "VP":
             resultado = lista_paises.search_item(commands[1])
             if resultado == False:
@@ -37,14 +45,17 @@ def main():
             else:
                 print(f"O país {commands[1]} encontra-se na lista.")
 
+        #Eliminar o primeiro país
         if commands[0] == "EPE":
             print(f"O país {lista_paises.start_node.item} foi eliminado da lista.")
             lista_paises.delete_at_start()
 
+        #Eliminar o ultimo país
         if commands[0] == "EUE":
             print(f"O país {lista_paises.get_last_node()} foi eliminado da lista.")
             lista_paises.delete_at_end()
 
+        #Eliminar um país espexifico
         if commands[0] == "EP":
             if lista_paises.search_item(commands[1]) == False:
                 print(f"O país {commands[1]} não se encontra na lista.")
